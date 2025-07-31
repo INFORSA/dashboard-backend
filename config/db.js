394f32f0ac116891141b3a612f2b82dd92cd05aa
zackoflonely <zakifauzanrabb@gmail.com>
2025-07-31 21:49:05 +0800

    add env config

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // load file .env
 
 // Buat koneksi ke database
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'inforsa',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Cek koneksi
